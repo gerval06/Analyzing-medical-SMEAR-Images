418c83fba3960c4ec625fc9a0c99fa813b702765
gerval06 <Gerhard.Valcl@gmx.at>
2017-01-12 15:28:37 +0100

    Addidtion of various functions and documentation. Project finished.

--- Analysing_SMEAR_Images.m
+++ Analysing_SMEAR_Images.m
@@ -1,12 +1,13 @@
 %% Analysing SMEAR Images
 % Description:
 % Analysing and extracting information out of SMEAR Images.
-%
 
 clc
 clear all
 close all hidden
 %% Loading and showing data
+% Loading data with |uigetfile| function. Loading is done depending on
+% file index.
 
 cd .\Data
 [Filename,~,FilterIndex] = uigetfile({'*.mat';'*.jpg';'*.bmp'},'Select File');
@@ -24,15 +25,12 @@ imshow(img);
 
 % redHist = imhist(img(:,:,1));
 % redHistMean = mean(redHist);
-% if (redHistMean < (1.0594E+03 - (706.2978))) || (redHistMean > (1.0594E+03 - (706.2978)))
+% if ~(redHistMean < (1.0594E+03 - (706.2978))) || (redHistMean > (1.0594E+03 + (706.2978)))
 %     boxH = msgbox('Quality of Picture is to low. Program stops !','Error','error');
 %     return
 % end
 %% Analysing Image
-% Test bwboundaries function to find boundaries of croped image to get
-% cropped image use imcrop function.
-% 
-% Make black and white image for further analysis
+% Adjusting Image by improving background and adjusting background
 
 img_gray = rgb2gray(img);
 background = imopen(img_gray,strel('disk',40));
@@ -42,21 +40,13 @@ img_adjust = imadjust(img_gray_comp);
 
 background2 = imopen(img_adjust,strel('disk',15));
 img_adjust2 = img_adjust - background;
-%imshow(im_adjust2)
 
-level = graythresh(img_adjust2);
-bw = im2bw(img_adjust2,level);
-bw_fill = imfill(bw,'holes');
+%% Counting circles
+% Defining min and max radii and using Hough transform for detecting
+% circles. 
 
-% Test algorithm with |bwconncomp|
-% cc = bwconncomp(bw_fill,4);
-% cc.NumObjects
 RadMin = 10;
 RadMax = 25;
-if verLessThan('matlab','8.3.0.532')
-    [CircleCenter, CircleRad] = CountCircles(img_adjust, RadMin, RadMax);
-else
-    [CircleCenter, CircleRad] = CountCircles(bw_fill, RadMin, RadMax);
-end
-circ_h = viscircles(CircleCenter, CircleRad,'EdgeColor','b');
+[CircleCenter, CircleRad] = CountCircles(img_adjust, RadMin, RadMax);
+%circ_h = viscircles(CircleCenter, CircleRad,'EdgeColor','b');
 %% check if white blood cells where counted aswell
